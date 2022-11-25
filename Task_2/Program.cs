0f1592825ecdbf61152117e970474130b4c30908
Zhuravlev84 <zyzy84@rambler.ru>
2022-11-25 06:48:49 +0300

    finish

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число
+ и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int number = ReadInt ("Введите число: ");
+
+int temp = number;
+int a = 0;
+while (temp > 0)
+{
+    temp = temp/10;
+    a++;
+}
+a = a - 1;
+int sum = number%10;
+while (a > 0)
+{
+    number = number/10;
+    sum = sum + number % 10;
+    a = a - 1;
+}
+Console.Write (sum);
+int ReadInt (string messege)
+{   
+    Console.Write (messege);
+    return Convert.ToInt32 (Console.ReadLine());
+}
